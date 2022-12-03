ce773e5b9c5f1884a848aa7915b57d70eaeb66af
anccode <cj_angel@outlook.com>
2022-12-03 10:33:47 -0500

    arreglando errores

--- pages/api/v1/attendances/index.ts
+++ pages/api/v1/attendances/index.ts
@@ -1,15 +1,15 @@
 import { NextApiRequest, NextApiResponse } from "next";
-import { verifyToken } from "../../../../middlewares/authJwt";
+//import { verifyToken } from "../../../../middlewares/authJwt";
 import { ModelAsistencia } from "../../../../models";
 
 // eslint-disable-next-line import/no-anonymous-default-export
 export default async (req: NextApiRequest, res: NextApiResponse) => {
   const { method } = req;
-  verifyToken(req,res)
+  //verifyToken(req,res)
   switch (method) {
     case "GET":
       try {
-        const [attendances] = await ModelAsistencia.findAll();
+        const attendances = await ModelAsistencia.findAll();
         return res.status(200).json(attendances);
       } catch (error) {
         return res.status(500).json({ message: error });
